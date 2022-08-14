e1143ec456b69d6d0d92dc911be716203b471704
Muhammad Jawad <jawad.topdot@gmail.com>
2022-08-14 20:53:36 +0500

    Added update task code

--- app/Http/Controllers/Api/V1/Workflow/Tasks/UpdateController.php
+++ app/Http/Controllers/Api/V1/Workflow/Tasks/UpdateController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1\Workflow\Tasks;
 
-use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Workflow\Tasks\StoreRequest;
+use App\Http\Resources\TaskResource;
 
 class UpdateController extends Controller
 {
@@ -13,8 +18,13 @@ class UpdateController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(StoreRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+
+        return new JsonResponse(
+            data: TaskResource::make($task),
+            status: Response::HTTP_OK,
+        ); 
     }
 }
